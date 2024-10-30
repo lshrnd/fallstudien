18da71df968fb5f2303831b709dd7d78472f3051
KathaWes <119424887+KathaWes@users.noreply.github.com>
2024-10-30 14:36:05 +0100

    Add files via upload

---
+++ Projekt 2/Projekt 2_Katharina.R
@@ -0,0 +1,27 @@
+data<- read.table("Konzentrationsdaten.txt", header = T)
+mar_def = c(5, 4, 4, 2) + 0.1 # Default-Margin
+
+#Nur Daten von Durchgang 1
+datad1<-data[data$durchgang==1,]
+
+#Deskription
+
+#Gemeinsames Histogramm für KL nach beiden Gruppen
+par(mar=c(4.5,4.5,1,0.5))
+hist(datad1$KL[datad1$gruppe==1], breaks = 20, xlim =c(-25,20),
+     col=rgb(1,0,0,0.5), main ="", xlab = "Konzentrationsleistung", ylab="Häufigkeit",
+     cex.axis=1.25, cex.lab=1.75)
+hist(datad1$KL[datad1$gruppe==2], breaks = 5, xlim =c(-25,20), col=rgb(0,0,1,0.5), add=T)
+axis(1, at = seq(-35, 20, by = 5), cex.axis = 1.25)
+legend("topleft", legend=c("Gruppe 1", "Gruppe 2"), col=c(rgb(1,0,0,0.5),rgb(0,0,1,0.5)),
+       pt.cex = 2, pch = 15, cex =1.75, bty="n", x.intersp =0.25)
+
+#Gemeinsames Histogramm für Bearbeitungszeit nach Gruppe
+par(mar=c(4.5,4.5,1,0.5))
+hist(datad1$B[datad1$gruppe==1], breaks = 10, xlim =c(50,225),
+     col=rgb(1,0,0,0.5), main ="", xlab = "Bearbeitungszeit in Sekunden", ylab="Häufigkeit",
+     cex.axis=1.25, cex.lab=1.75)
+hist(datad1$B[datad1$gruppe==2], breaks = 10, xlim =c(50,225), col=rgb(0,0,1,0.5), add=T)
+axis(1, at = seq(50, 225, by = 25), cex.axis = 1.25)
+legend("topright", legend=c("Gruppe 1", "Gruppe 2"), col=c(rgb(1,0,0,0.5),rgb(0,0,1,0.5)),
+       pt.cex = 2, pch = 15, cex =1.75, bty="n", x.intersp =0.5)
