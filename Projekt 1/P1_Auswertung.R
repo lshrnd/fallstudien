07b189249b974632898e85eca565061d61865061
nils-mac <96783719+nils-mac@users.noreply.github.com>
2024-10-30 21:43:20 +0100

    Revert "Create P1_Nils.R"
    This reverts commit 2fa1e1510d62e6d9afaebff35930894f9a9b98d9.

--- Projekt 1/P1_Auswertung.R
+++ Projekt 1/P1_Auswertung.R
@@ -11,6 +11,9 @@ data02 = data[data$Year == 2002,]
 data02_clean = na.omit(data02) 
 data22_clean = semi_join(data22, data02_clean, by= "Country")
 # Daten wenn beide Jahre existieren (fuer 4)
+data_merge_clean<-merge(data02_clean, data22_clean, by= "Country",
+                        suffixes = c("_2002","_2022"))
+data_merge_clean<-data_merge_clean[, -c(2,7:9)]
 
 #### allgemeine Variablen:
 mar_def = c(5, 4, 4, 2) + 0.1 # Default-Margin
@@ -21,27 +24,26 @@ region_names = c("Asien", "Europa", "Afrika", "Ozeanien", "Amerika")
 ####
 # Box-Plots der drei Lebenserwartung-Kennzahlen von 2022
 boxplot(data22$Life_Expectancy_Overall, data22$Life_Expectancy_Male, data22$Life_Expectancy_Female, 
-        names = c("Gesamt","Männlich","Weiblich"), las = 1, cex.axis = 0.7, main = "Boxplot der Lebenserwartungen aller Länder",
+        names = c("Gesamt","Männlich","Weiblich"), las = 1, cex.axis = 0.7,
         xlab = "Alter in Jahre", horizontal = T)
 
 
 # Bar-Plot der Haeufigkeiten der Regionen/Subregionen
 par(mar=c(4, 5, 2.5, 2))
 RegionTable = sort(table(data22$Region), decreasing = F)
-barplot(RegionTable, col = alpha(region_cols[names(RegionTable)],0.25), xlim = c(0,60), horiz = T, las = 2,
-        main = "Anzahl der Länder innerhalb einer Region")
+barplot(RegionTable, col = alpha(region_cols[names(RegionTable)],0.25), xlim = c(0,60), horiz = T, las = 2)
 par(mar=mar_def)
 
 par(mar=c(4, 9, 2.5, 2))
 SubregionTable = sort(table(data22$Subregion), decreasing = F)
 barplot(SubregionTable, col = alpha(region_cols[data22$Region[match(names(SubregionTable), data22$Subregion)]],0.25),
-        las = 2, cex.names = 0.9, horiz = T, main = "Anzahl der Länder innerhalb einer Subregion")
-legend(21, 15, legend = region_names, pt.bg = region_cols, pch = 22, col = "black", 
-       pt.cex = 1.3, text.width = 2, x.intersp = 0.7, y.intersp = 0.8)
+        las = 2, cex.names = 0.9, horiz = T)
+legend(19, 15, legend = region_names, pt.bg = alpha(region_cols, 0.25), pch = 22, col = "black", 
+       pt.cex = 1.3, text.width = 2, x.intersp = 0.6, y.intersp = 0.8)
 par(mar=mar_def)
 
 # Box-Plot der Geburtenrate in 2022
-boxplot(data22$Total_Fertility_Rate, horizontal = T, main = "Geburtenrate in 2022")
+boxplot(data22$Total_Fertility_Rate, horizontal = T)
 
 
 #### 4 - Jahresvergleich
@@ -49,13 +51,12 @@ Life_Expectancy_Change = (data22_clean$Life_Expectancy_Overall - data02_clean$Li
 # Histogramm der Veraenderung der Lebenserwartung
 histcols = alpha(c("red","red",rep("green",22)),0.15)
 hist(Life_Expectancy_Change + 1, xlim = c(0.9,1.5), breaks = 20, col = histcols,
-     xlab = "Anteil der vorherigen Lebenserwartung, 1.1 = +10%", ylab = "absolute Häufigkeit", main = "Veränderung der Lebenserwartung", )
+     xlab = "Anteil der vorherigen Lebenserwartung, 1.1 = +10%", ylab = "absolute Häufigkeit")
   # TODO: xlab / main muss verbessert werden, so nicht aussagekräftig genug
 
 # Scatterplot der Lebenserwartung in 2002 gegen die Veraenderung der Lebensrate
 plot(data02_clean$Life_Expectancy_Overall,Life_Expectancy_Change, bg = alpha(region_cols[data02_clean$Region],0.7), pch = 21, col = "darkgrey",
-     xlab = "Lebenserwartung in 2002 in Jahren", ylab = "Veränderung in Prozent", 
-     main = "prozentuale Veränderung der Lebenserwartung von 2002 zu 2022")
+     xlab = "Lebenserwartung in 2002 in Jahren", ylab = "Veränderung in Prozent")
 legend(82, 0.43, legend = region_names, pt.bg = region_cols, pch = 21, col = "black", 
        pt.cex = 1.3, text.width = 4, x.intersp = 0.7, y.intersp = 0.8)
 
@@ -64,10 +65,48 @@ legend(82, 0.43, legend = region_names, pt.bg = region_cols, pch = 21, col = "bl
 plot(data02_clean$Life_Expectancy_Overall,data22_clean$Life_Expectancy_Overall,
       #pch = region_symb[data02_clean$Region], col = alpha(region_cols[data02_clean$Region],0.7), 
      bg = alpha(region_cols[data02_clean$Region],0.7), pch = 21, col ="darkgrey",
-     xlim = c(45,90), ylim = c(45,90), xlab = "Lebenserwartung in 2002 in Jahren", ylab = "Lebenserwartung in 2022 in Jahren", 
-     main = "Lebenserwartung von 2002 und 2022 im Vergleich")
+     xlim = c(45,90), ylim = c(45,90), xlab = "Lebenserwartung in 2002 in Jahren",
+     ylab = "Lebenserwartung in 2022 in Jahren")
 points(45:90, 45:90, type = "l", col = grey(0,0.3) )
 legend("topleft", legend = c(region_names,"Diagonale"),
        #pch = c(16,17,18,20,19,NA),lty = c(rep(NA,5),1), col = c(region_cols,"darkgrey"),
        pch = c(rep(21,5),NA), lty = c(rep(NA,5),1), pt.bg = region_cols, col = c(rep("black",5),"darkgrey"),
        pt.cex = 1.1, text.width = 4, x.intersp = 0.7, y.intersp = 0.8)
+
+#Dotchart für die Lebenserwatung im Vergleich 2002 gegen 2022
+#Farblich Regionale Aufteilung auskommentiert
+data_merge_clean<-data_merge_clean[order(
+  data_merge_clean$Life_Expectancy_Overall_2002),]
+dotchart(data_merge_clean$Life_Expectancy_Overall_2002, 
+         xlab = "Lebenserwartung in Jahren", ylab = "Länder",
+         pch = 1, cex = 0.7,
+         xlim = range(c(data_merge_clean$Life_Expectancy_Overall_2022,
+                        data_merge_clean$Life_Expectancy_Overall_2002)),
+         lcolor = "White",
+           col = alpha(region_cols[data_merge_clean$Region_2022],0.7)
+         )
+points(data_merge_clean$Life_Expectancy_Overall_2022,
+       1:nrow(data_merge_clean),  pch=16,
+         col = alpha(region_cols[data_merge_clean$Region_2022],0.7)
+)
+#legend("topleft", legend = c("2002", "2022"), pch = c(1,16))
+legend("left", legend = region_names, fill = alpha(region_cols, 0.7),title = "Regionen")
+
+#Dotchart Fertility im Vergleich 2002 gegen 2022
+# Farbliche Aufteilung nach Region auskommentiert
+data_merge_clean<-data_merge_clean[order(
+  data_merge_clean$Total_Fertility_Rate_2002),]
+dotchart(data_merge_clean$Total_Fertility_Rate_2002,
+         xlab = "Fertilitätsrate", pch=1,
+         xlim = range(c(data_merge_clean$Total_Fertility_Rate_2022,
+                        data_merge_clean$Total_Fertility_Rate_2002)),
+         ylab = "Länder", 
+         lcolor = "White",
+        # col = alpha(region_cols[data_merge_clean$Region_2022],0.7)
+         )
+points(data_merge_clean$Total_Fertility_Rate_2022,
+       1:nrow(data_merge_clean),  pch=16,
+       #col = alpha(region_cols[data_merge_clean$Region_2022],0.7)
+       )
+legend("right", legend = c("2002", "2022"), pch = c(1,16), title = "Jahr")
+#legend("bottomright", legend = region_names, fill = alpha(region_cols, 0.7),title = "Regionen")
--- Projekt 1/P1_Nils.R
+++ Projekt 1/P1_Nils.R
@@ -19,9 +19,27 @@ zen22 <- na.omit(census22)
 
 # Boxplots Lebenserwartung nach Regionen und Subregionen
 
-boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Europe"], zen22$Life_Expectancy_Overall[zen22$Region == "Asia"], zen22$Life_Expectancy_Overall[zen22$Region == "Americas"], zen22$Life_Expectancy_Overall[zen22$Region == "Africa"], zen22$Life_Expectancy_Overall[zen22$Region == "Oceania"], names=c("Europe", "Asia", "Americas", "Africa", "Oceania"))
+boxplot(data22$Life_Expectancy_Overall, data22$Life_Expectancy_Male, data22$Life_Expectancy_Female, 
+        names = c("Gesamt","Männlich","Weiblich"), las = 1, cex.axis = 0.7,
+        xlab = "Alter in Jahre", horizontal = T)
 
-boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Northern Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Western Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Eastern Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Southern Europe"], names=c("Europa", "Nord", "West", "Ost", "Süd"))
+?boxplot
+
+boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Europe"], zen22$Life_Expectancy_Overall[zen22$Region == "Asia"], zen22$Life_Expectancy_Overall[zen22$Region == "Americas"], zen22$Life_Expectancy_Overall[zen22$Region == "Africa"], zen22$Life_Expectancy_Overall[zen22$Region == "Oceania"], names=c("Europa", "Asien", "Amerika", "Afrika", "Ozeanien"),las = 1, cex.axis = 0.7,
+        xlab = "", ylab ="", horizontal = T)
+title(xlab = "Alter in Jahre", line = 2.5)
+title(ylab = "Kontinente", line = 4)
+
+
+# Europa
+boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Northern Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Western Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Eastern Europe"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Southern Europe"], names=c("Europa", "Nordeuropa", "Westeuropa", "Osteuropa", "Südeuropa"),las = 1, cex.axis = 0.7,
+        xlab = "", ylab ="", horizontal = T, at = c(1,2.5,3.5,4.5,5.5))
+abline(h = 1.75,lty = 3)
+title(xlab = "Alter in Jahre", line = 2.5)
+title(ylab = "(Sub-)Regionen", line = 4.5)
+
+
+
 boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Asia"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Western Asia"], zen22$Life_Expectancy_Overall[zen22$Subregion == "South-Central Asia"], zen22$Life_Expectancy_Overall[zen22$Subregion == "South-Eastern Asia"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Eastern Asia"], names=c("Asien", "West", "Süd-Zentral", "Süd-Ost", "Ost"))
 boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Americas"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Northern America"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Central America"], zen22$Life_Expectancy_Overall[zen22$Subregion == "South America"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Caribbean"], names=c("Amerika", "Nord", "Zentral", "Süd", "Karibik"))
 boxplot(zen22$Life_Expectancy_Overall[zen22$Region == "Africa"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Northern Africa"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Middle Africa"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Southern Africa"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Western Africa"], zen22$Life_Expectancy_Overall[zen22$Subregion == "Eastern Africa"], names=c("Afrika", "Nord", "Mittel", "Süd", "West", "Ost"))
@@ -60,20 +78,88 @@ head(map_zen22) # überprüfen ob Umbenennung funktioniert hat
 
 #Erstellen eines Datensatzes mit Koordinaten zu den einzelnen Ländern
 mapdata <-map_data("world")
-view(mapdata)
+#view(mapdata)
+
+#Länder nach Census benennen
+mapdata$subregion[mapdata$region == "Antigua"] <- "Antigua"
+mapdata$region[mapdata$region == "Antigua"] <- "Antigua and Barbuda"
+mapdata$subregion[mapdata$region == "Barbuda"] <- "Barbuda"
+mapdata$region[mapdata$region == "Barbuda"] <- "Antigua and Barbuda"
+mapdata$region[mapdata$region == "Bahamas"] <- "Bahamas, The"
+mapdata$region[mapdata$region == "Myanmar"] <- "Burma"
+mapdata$region[mapdata$region == "Cape Verde"] <- "Cabo Verde"
+mapdata$region[mapdata$region == "Republic of Congo"] <- "Congo (Brazzaville)"
+mapdata$region[mapdata$region == "Democratic Republic of the Congo"] <- "Congo (Kinshasa)"
+mapdata$region[mapdata$region == "Ivory Coast"] <- "Côte d'Ivoire"
+mapdata$region[mapdata$region == "Curacao"] <- "Curaçao"
+mapdata$region[mapdata$region == "Czech Republic"] <- "Czechia"
+mapdata$region[mapdata$region == "Swaziland"] <- "Eswatini"
+mapdata$subregion[mapdata$region == "French Guiana"] <- "French Guiana"
+mapdata$region[mapdata$region == "French Guiana"] <- "France"
+mapdata$region[mapdata$region == "French Southern and Antarctic Lands"] <- "France"
+mapdata$region[mapdata$region == "Guadeloupe"] <- "France"
+mapdata$subregion[mapdata$region == "Martinique"] <- "Martinique"
+mapdata$region[mapdata$region == "Martinique"] <- "France"
+mapdata$subregion[mapdata$region == "Mayotte"] <- "Mayotte"
+mapdata$region[mapdata$region == "Mayotte"] <- "France"
+mapdata$subregion[mapdata$region == "Reunion"] <- "Reunion"
+mapdata$region[mapdata$region == "Reunion"] <- "France"
+mapdata$region[mapdata$region == "Gambia"] <- "Gambia, The"
+mapdata$region[mapdata$subregion == "Gaza Strip"] <- "Gaza Strip"
+mapdata$subregion[mapdata$subregion == "Gaza Strip"] <- NA
+mapdata$region[mapdata$subregion == "Hong Kong"] <- "Hong Kong"
+mapdata$subregion[mapdata$subregion == "Hong Kong"] <- NA
+mapdata$region[mapdata$region == "North Korea"] <- "Korea, North"
+mapdata$region[mapdata$region == "South Korea"] <- "Korea, South"
+mapdata$region[mapdata$subregion == "Macao"] <- "Macau"
+mapdata$subregion[mapdata$subregion == "Macao"] <- NA
+mapdata$region[mapdata$region == "Micronesia"] <- "Micronesia, Federated States of"
+mapdata$subregion[mapdata$region == "Azores"] <- "Azores"
+mapdata$region[mapdata$region == "Azores"] <- "Portugal"
+mapdata$region[mapdata$region == "Madeira Islands"] <- "Portugal"
+mapdata$subregion[mapdata$region == "Ascension Island"] <- "Ascension Island"
+mapdata$region[mapdata$region == "Ascension Island"] <- "Saint Helena, Ascension, and Tristan da Cunha"
+mapdata$subregion[mapdata$region == "Saint Helena"] <- "Saint Helena"
+mapdata$region[mapdata$region == "Saint Helena"] <- "Saint Helena, Ascension, and Tristan da Cunha"
+mapdata$subregion[mapdata$region == "Nevis"] <- "Nevis"
+mapdata$region[mapdata$region == "Nevis"] <- "Saint Kitts and Nevis"
+mapdata$subregion[mapdata$region == "Saint Kitts"] <- "Saint Kitts"
+mapdata$region[mapdata$region == "Saint Kitts"] <- "Saint Kitts and Nevis"
+mapdata$region[mapdata$region == "Grenadines"] <- "Saint Vincent and the Grenadines"
+mapdata$subregion[mapdata$region == "Saint Vincent"] <- "Saint Vincent"
+mapdata$region[mapdata$region == "Saint Vincent"] <- "Saint Vincent and the Grenadines"
+mapdata$region[mapdata$region == "Canary Islands"] <- "Spain"
+mapdata$subregion[mapdata$region == "Tobago"] <- "Tobago"
+mapdata$region[mapdata$region == "Tobago"] <- "Trinidad and Tobago"
+mapdata$subregion[mapdata$region == "Trinidad"] <- "Trinidad"
+mapdata$region[mapdata$region == "Trinidad"] <- "Trinidad and Tobago"
+mapdata$region[mapdata$region == "UK"] <- "United Kingdom"
+mapdata$region[mapdata$region == "USA"] <- "United States"
+mapdata$region[mapdata$region == "Virgin Islands" & mapdata$subregion == " British"] <- "Virgin Islands, British"
+mapdata$subregion[mapdata$region == "Virgin Islands" & mapdata$subregion == " British"] <- NA
+mapdata$region[mapdata$region == "Virgin Islands" & mapdata$subregion == " US"] <- "Virgin Islands, U.S."
+mapdata$subregion[mapdata$region == "Virgin Islands" & mapdata$subregion == " US"] <- NA
+mapdata$region[mapdata$region == "Palestine" & mapdata$subregion == "2"] <- "West Bank"
+mapdata$subregion[mapdata$region == "West Bank"] <- NA
+
+
 # beide Datensätze in einem neuen Datensatz vereinen
 alldata <- left_join(mapdata, map_zen22, by="region")
-view(alldata)
+#view(alldata)
 
-map1<-ggplot(alldata[alldata$continent == "Oceania",], aes(x= long, y = lat, group=group))+
-  geom_polygon(aes(fill =Life_Expectancy_Overall),color="black")
+# Lebenserwartung in Kategorien einteilen
+alldata$kategorie <- cut(alldata$Life_Expectancy_Overall,
+                         breaks = c(0, 50, 60, 70, 80, 100),
+                         labels = c("x<50", "50<=x<60", "60<=x<70", "70<=x<80", "80<=x"))
+
+map1<-ggplot(alldata, aes(x= long, y = lat, group=group))+
+  #geom_polygon(aes(fill =Life_Expectancy_Overall),color="black")
+  geom_polygon(aes(fill =kategorie),color="black")
 map1
 
 #Optische Anpassung der Weltkarte
-map2<-map1 + scale_fill_gradient(name="Jahre", 
-                                 low ="red", 
-                                 high="green", 
-                                 na.value="grey50")+
+#map2<-map1 + scale_fill_gradient(name="Jahre", low ="orange", high="firebrick4", na.value="grey50")+
+  map2<-map1 + scale_fill_manual(values = c("x<50" = "red", "50<=x<60" = "red", "60<=x<70" = "yellow", "70<=x<80" = "green", "80<=x" = "blue"))+
   labs(title = "Lebenserwartung bei Geburt im Jahr 2022 nach Ländern", 
        subtitle = "(bei grau unterlegten Ländern sind keine Daten verfügbar)")+
   theme(axis.text.x = element_blank(),
