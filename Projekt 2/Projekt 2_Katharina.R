2b7759c2768d46a7f28dc0833cba87e6efa3599e
KathaWes <119424887+KathaWes@users.noreply.github.com>
2024-11-03 22:21:12 +0100

    Ergänzung Shapiro-Francia Test

--- Projekt 2/Projekt 2_Katharina.R
+++ Projekt 2/Projekt 2_Katharina.R
@@ -25,3 +25,38 @@ hist(datad1$B[datad1$gruppe==2], breaks = 10, xlim =c(50,225), col=rgb(0,0,1,0.5
 axis(1, at = seq(50, 225, by = 25), cex.axis = 1.25)
 legend("topright", legend=c("Gruppe 1", "Gruppe 2"), col=c(rgb(1,0,0,0.5),rgb(0,0,1,0.5)),
        pt.cex = 2, pch = 15, cex =1.75, bty="n", x.intersp =0.5)
+
+############################################################################## 
+##############################################################################  
+# Testen, ab hier mit Bereinigung des Ausreissers
+data[which(data$id == 14),]
+
+dataN = data[- which(data$id == 14),]
+
+dataN_g1 = dataN[dataN$gruppe == 1,]
+dataN_g2 = dataN[dataN$gruppe == 2,]
+
+dataN_d1 = dataN[dataN$durchgang == 1,]
+dataN_d2 = dataN[dataN$durchgang == 2,]
+
+
+## Aufgabe 1: durchgang 1 - gruppen unterscheidung 
+### Test 1 shapiro auf normalverteilungsannahme
+shapiro.test(dataN_g1$KL[dataN_g1$durchgang == 1])
+shapiro.test(dataN_g2$KL[dataN_g2$durchgang == 1])
+
+
+#Shapiro-Francia
+shapiro.francia<-function(x){
+W <- cor(sort(x),
+         qnorm(ppoints(x, a = 3/8)))^2
+u <- log(length(x))
+v <- log(u)
+alpha <- -1.2725 + 1.0521 * (v - u)
+beta <- 1.0308 - 0.26758 * (v + 2/u)
+Z<- (log(1 - W) - alpha)/beta
+pnorm(Z, lower.tail = FALSE)
+}
+#Testen auf Normalverteilungsannahme mit shaprio.francia
+shapiro.francia(dataN_g1$KL[dataN_g1$durchgang == 1])
+shapiro.francia(dataN_g2$KL[dataN_g2$durchgang == 1])
