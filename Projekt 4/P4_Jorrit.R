d15b60cfbe17d6620ebb1f32d5f9ebbb46d85d9a
notabrak <124277845+JKuehne-TU@users.noreply.github.com>
2024-12-12 16:32:42 +0100

    Changed my Code and added literature

--- Projekt 4/P4_Jorrit.R
+++ Projekt 4/P4_Jorrit.R
@@ -16,9 +16,11 @@ sportarten = unique(data$Sportart)
 ## der Gesamtanzahl an Medaillen
 
 cTable1 = t(matrix(data$Total, nrow = 4, ncol = 5))
-
 rownames(cTable1) = laender
 colnames(cTable1) = sportarten
+
+chisq.test(cTable1)
+chisq.test(cTable1, simulate.p.value = TRUE)
 
 
 ### Notizen Aufgabe 2
@@ -30,22 +32,39 @@ colnames(cTable1) = sportarten
 
 cTableKampfsport = as.matrix(subset(data, Sportart == "Kampfsport")[ , 3:5])
 rownames(cTableKampfsport) = laender
+sum(cTableKampfsport)
+chisq.test(cTableKampfsport)
+chisq.test(cTableKampfsport, simulate.p.value = TRUE)
+
 
 # Leichtathletik
 
 cTableLeichtathletik = as.matrix(subset(data, Sportart == "Leichtathletik")[ , 3:5])
 rownames(cTableLeichtathletik) = laender
+sum(cTableLeichtathletik)
+chisq.test(cTableLeichtathletik)
+chisq.test(cTableLeichtathletik, simulate.p.value = TRUE)
+
 
 # Ballsportarten
 
 cTableBallsportarten = as.matrix(subset(data, Sportart == "Ballsportart")[ , 3:5])
 rownames(cTableBallsportarten) = laender
+sum(cTableBallsportarten)
+chisq.test(cTableBallsportarten)
+chisq.test(cTableBallsportarten, simulate.p.value = TRUE)
 
 # Schwimmen
 
 cTableSchwimmen = as.matrix(subset(data, Sportart == "Schwimmen")[ , 3:5])
 rownames(cTableSchwimmen) = laender
+sum(cTableSchwimmen)
+chisq.test(cTableSchwimmen)
+chisq.test(cTableSchwimmen, simulate.p.value = TRUE)
 
+
+# Voraussetzung der Unabhänigigen Stichprobe wahrscheinlich für diesen Aufgabenteil
+# nicht gegeben, da die Zeilen (Medaillenfarben für Land) definitiv abhängig sind.
 
 
 ### Notizen Aufgabe 3
@@ -57,23 +76,46 @@ rownames(cTableSchwimmen) = laender
 
 cTableUSA = as.matrix(subset(data, Land == "USA")[ , 3:5])
 rownames(cTableUSA) = sportarten
+sum(cTableUSA)
+chisq.test(cTableUSA)
 
 # VR China
 
 cTableVRChina = as.matrix(subset(data, Land == "VR China")[ , 3:5])
 rownames(cTableVRChina) = sportarten
+sum(cTableVRChina)
+chisq.test(cTableVRChina)
 
 # Japan
 
 cTableJapan = as.matrix(subset(data, Land == "Japan")[ , 3:5])
 rownames(cTableJapan) = sportarten
+sum(cTableJapan)
+chisq.test(cTableJapan)
 
 # Australien
 
 cTableAustralien = as.matrix(subset(data, Land == "Australien")[ , 3:5])
 rownames(cTableAustralien) = sportarten
+sum(cTableAustralien)
+chisq.test(cTableAustralien)
 
 # Frankreich
 
 cTableFrankreich = as.matrix(subset(data, Land == "Frankreich")[ , 3:5])
 rownames(cTableFrankreich) = sportarten
+sum(cTableFrankreich)
+chisq.test(cTableFrankreich)
+
+
+
+permutationChisqTest = function(cTable) {
+  newCols = sample(length(cTable[1, ]))
+  print(newCols)
+  newCTable = transform(as.data.frame(cTable), newCols)
+  newCTable
+}
+
+permutationChisqTest(cTableFrankreich)
+
+
